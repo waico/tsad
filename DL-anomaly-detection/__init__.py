fa41bad412702cf26590214e0c366508a4735e96
Viacheslav Kozitsin <rfptk2525@yandex.ru>
2021-03-21 20:02:09 +0300

    add docstring

--- DL-anomaly-detection/__init__.py
+++
@@ -1,3 +0,0 @@
-from .diff_integ import diff_integ
-from .find_best_model import find_best_model
-from .tanh import anomaly_detection, online_tanh, get_score
--- DL-anomaly-detection/generate_residuals.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def generate_res_func(y_pred,y_true):
-    assert y_pred.shape == y_true.shape
-    return np.abs(y_pred-y_true)
