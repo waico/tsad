41aef94bc18a72e8b8ecce10f27d97a4ab6ea177
Oleg Berezin <telepzk@gmail.com>
2023-09-04 18:59:33 +0700

    Add basic tests

---
+++ tests/test_pipeline.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import pandas as pd
+
+from tsad.base.pipeline import Pipeline
+from tsad.base.task import Task, TaskResult
+
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+TSAD_DIR = os.path.abspath(os.path.join(TESTS_DIR, os.pardir))
+sys.path.insert(0, TSAD_DIR)
+
+
+class FirstTestTaskResult(TaskResult):
+
+    length: int
+
+    def show():
+        pass
+
+
+class FistTestTask(Task):
+    
+
+    def fit(self, df: pd.DataFrame) -> tuple[pd.DataFrame, TaskResult]:
+        result = FirstTestTaskResult()
+        result.length = len(df)
+        return df, result
+
+
+    def predict(self, df: pd.DataFrame, fit_result: FirstTestTaskResult) -> tuple[pd.DataFrame, TaskResult]:
+        assert len(df) == fit_result.length
+        return pd.DataFrame([1, 2, 3]), None
+
+
+def test_pipeline():
+    
+    df = pd.DataFrame([1, 2, 3, 4])
+
+    tasks = [FistTestTask()]
+
+    pipeline = Pipeline(tasks)
+
+    pipeline.fit(df)
+    predict_df = pipeline.predict(df)
+
+    assert len(predict_df) == 3
